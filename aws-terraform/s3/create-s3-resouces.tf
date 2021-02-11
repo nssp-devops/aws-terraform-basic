659b30761595dfa7824fbb09962531539798877b
Nitesh Gupta <nitesh.gupta@dimensiondata.com>
2021-02-11 16:55:12 +0530

    s3

---
+++ aws-terraform/s3/create-s3-resouces.tf
@@ -0,0 +1,33 @@
+#####################Provider###################
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+###################RESOURCE################
+
+resource "aws_s3_bucket" "b" {
+  bucket = "nssp0101-bucket11"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+###################Upload################
+# Upload an object
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.b.id
+  key    = "30-2.jpeg"
+  acl    = "public-read"  # or can be "public-read"
+  source = "./30-2.jpeg"
+  etag = filemd5("./30-2.jpeg")
+}
+
+
+#####################OUTPUT###############
+output "s3-bucket-url" {
+    value = "${aws_s3_bucket.b}"  
+}
