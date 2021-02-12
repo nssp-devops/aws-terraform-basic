9f91ee4865460c0339e67df536de729eb376188a
Srinivas Ashok <srinivas.ashok@cis.ntt.com>
2021-02-12 12:49:29 +0530

    added coutput

--- aws-terraform/s3_mgmt/create_s3_bucket.tf
+++ aws-terraform/s3_mgmt/create_s3_bucket.tf
@@ -52,3 +52,11 @@ resource "aws_s3_bucket_object" "nssp_folder" {
     source = "./folder1/srini.txt"
     etag = filemd5("./folder1/srini.txt")
 }
+
+output "aws_s3_bucket_url"  {
+  value = "${formatlist(
+    "%s/%s", 
+    "https://${aws_s3_bucket.s3_b.bucket_domain_name}",
+    "${aws_s3_bucket_object.nssp_folder.key}"
+  )}"
+}
